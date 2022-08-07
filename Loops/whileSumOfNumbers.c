b15e7d5dfa7c02d59f4d6f212a7237db2657064a
JoroKirilov <Gkirilov1987@abv.bg>
2022-08-07 13:50:11 +0300

    Not complete

---
+++ Loops/whileSumOfNumbers.c
@@ -0,0 +1,27 @@
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main()
+{
+   int i = 12;
+   int rezult = 0 ; 
+    
+   while (i <= 24)
+   {
+       int rezult = rezult + i ; 
+      /* printf("%d\n",rezult); */
+       i++;
+   }
+   
+   printf ("%d" , rezult);
+      
+    
+    
+        
+ 
+
+return 0;
+}
