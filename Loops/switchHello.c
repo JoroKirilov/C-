8c7f64c5584f2634dd6ecf578a406f901ba81ff6
JoroKirilov <Gkirilov1987@abv.bg>
2022-08-07 13:21:30 +0300

    switch

---
+++ Loops/switchHello.c
@@ -0,0 +1,58 @@
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int cases ;
+    scanf ("%d" , &cases);
+    
+    switch(cases)
+    {
+        case 1 :
+        printf("31");
+        break ; 
+        case 2 :
+        printf("29");
+        break ; 
+        case 3 :
+        printf("31");
+        break ; 
+         case 4 :
+        printf("31");
+        break ; 
+        case 5 :
+        printf("30");
+        break ; 
+        case 6 :
+        printf("31");
+        break ; 
+         case 7 :
+        printf("30");
+        break ; 
+        case 8 :
+        printf("31");
+        break ; 
+        case 9 :
+        printf("30");
+        break ; 
+         case 10 :
+        printf("31");
+        break ; 
+        case 11 :
+        printf("30");
+        break ; 
+        case 12 :
+        printf("31");
+        break ; 
+       
+        default : 
+        printf("Enter please number between 1 and 12 include ");
+        scanf ("%d" , &cases);
+        
+        
+    }
+
+    return 0;
+}
