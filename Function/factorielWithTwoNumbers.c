fa2d6fb0f1394e86a98c37ba6286333b403ac32d
JoroKirilov <Gkirilov1987@abv.bg>
2022-08-15 14:39:48 +0300

    Update when number is zero !!!

--- Function/factorielWithTwoNumbers.c
+++ Function/factorielWithTwoNumbers.c
@@ -3,24 +3,29 @@
 long int fact(int n);
 int main()
 {
-   int number ; 
-   int number1 ;
-   scanf("%d" , &number);
-   scanf("%d" , &number1);
-   printf("%ld\n%ld" , fact(number) , fact(number1));
-   
+    int number;
+    int number1;
+    scanf("%d", &number);
+    scanf("%d", &number1);
+    printf("%ld\n%ld", fact(number), fact(number1));
+
 
     return 0;
 }
 
-long int fact(int n )
+long int fact(int n)
 {
-    
-    long int factoriel = 1 ;
-    for (int i = 2 ; i <= n ; i++)
+    long int factoriel = 1;
+    if (n == 0)
     {
-        factoriel *= i ; 
+        factoriel = 0;
     }
-    return factoriel; 
-    
+    else
+    {
+        for (int i = 2; i <= n; i++)
+        {
+            factoriel *= i;
+        }
+        return factoriel;
+    }
 }
