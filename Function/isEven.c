0573f98ce044d96345f4bc0f221e44a895689899
JoroKirilov <Gkirilov1987@abv.bg>
2022-08-17 18:17:34 +0300

    add new solution with scanf for every number in array

--- Function/isEven.c
+++ Function/isEven.c
@@ -1,36 +1,41 @@
 #include <stdio.h>
 
-int isEven(int num)
-{
-    if(num % 2 == 0)
-    {
-    num= 1 ; 
-    }
-    else
-    {
-    num = 0 ; 
-    }
-    return num; 
-    
-}
+int isEven(int n);
+
 
 int main()
 {
-    int sumOfEvenOnly = 0 ;
-  int numbers[5];
-  for(int i = 0 ; i < 5 ; i++)
-  {
-      scanf("%d\n" , &numbers[i]);
-  }
-  for(int i = 0 ; i < 5 ; i++)
-  {
-       
-      if (isEven(numbers[i]) == 1)
-      {
-          sumOfEvenOnly += numbers[i];
-      }
-  }
-  printf("%d" , sumOfEvenOnly); 
-  
-   
+    int n;
+
+    scanf("%d", &n);
+    int evenNumber[n];
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int element;
+        scanf("%d", &element);
+        if (isEven(element) > 0)
+        {
+            evenNumber[i] = element;
+            sum += element;
+        }
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
+
+
+int isEven(int n)
+{
+    if (n % 2 == 0)
+    {
+        return n;
+    }
+    else
+    {
+        n = 0;
+        return n;
+    }
 }
