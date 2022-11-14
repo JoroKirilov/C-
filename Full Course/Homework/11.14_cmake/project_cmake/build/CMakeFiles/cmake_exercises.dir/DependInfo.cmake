5a9cbd842b730b61024f59ef1836a6e847e843b3
JoroKirilov <gkirilov1987@abv.bg>
2022-11-14 15:11:22 +0200

    add

---
+++ Full Course/Homework/11.14_cmake/project_cmake/build/CMakeFiles/cmake_exercises.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kirilov/code_workspace/C-/Full Course/Homework/11.14_cmake/project_cmake/main.c" "CMakeFiles/cmake_exercises.dir/main.c.o" "gcc" "CMakeFiles/cmake_exercises.dir/main.c.o.d"
+  "/home/kirilov/code_workspace/C-/Full Course/Homework/11.14_cmake/project_cmake/print.c" "CMakeFiles/cmake_exercises.dir/print.c.o" "gcc" "CMakeFiles/cmake_exercises.dir/print.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
