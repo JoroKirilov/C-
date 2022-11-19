7652bd06657b5ff1241f5c63258a06f0ad3fdce4
Georgi Kirilov <gkirilov1987@abv.bg>
2022-11-19 09:00:31 +0200

    Add files via upload

---
+++ Projects/Warcraft/cmake-build-debug/CMakeFiles/warcraft.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/warcraft.dir/src/main.c.o"
+  "warcraft.pdb"
+  "warcraft"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/warcraft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
